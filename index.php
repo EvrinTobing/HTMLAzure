3e1263e7f34995f4257aef16a40ecd6fb8c9a8a6
evrintobing <evrintobing@gmail.com>
2019-07-01 15:38:08 +0700

    update

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "dicodingservers.database.windows.net";
+    $user = "evrin";
+    $pass = "Asdewq123";
+    $db = "dcodedb";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
